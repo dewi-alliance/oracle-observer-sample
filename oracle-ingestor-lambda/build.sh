b618a7810131b0fb60f6b9819ef6dd14fd70fd66
luozhixin <871405986@qq.com>
2023-04-15 10:42:35 +0800

    fix: no such command: `lambda`

--- oracle-ingestor-lambda/build.sh
+++ oracle-ingestor-lambda/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf target lambda.zip bootstrap
-
+cargo install cargo-lambda
 cargo lambda build --release --arm64
 cp target/lambda/oracle-ingestor-lambda/bootstrap ./bootstrap
 zip lambda.zip bootstrap
